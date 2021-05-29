d16b01cb46f3d40e54852e136e29fe4211cc615d
minha <etchcure@gmail.com>
2021-05-29 13:44:08 +0900

    [리펙토링] 다크모드 토글 함수
    다크모드 토글 함수 전역에서 지역으로 변경

--- assets/js/custom/dark-theme.js
+++ assets/js/custom/dark-theme.js
@@ -1,6 +1,5 @@
 var defaultTheme = [...document.styleSheets].find(style => /(main.css)$/.test(style.href))
 var darkTheme = [...document.styleSheets].find(style => /(main_dark.css)$/.test(style.href))
-var changeTheme
 
 if (darkTheme) {
     const darkModeCookie = document.cookie
@@ -26,9 +25,11 @@ if (darkTheme) {
         toggleThemeBtn.checked = defaultTheme.disabled
     }
 
-    changeTheme = () => {
+    let changeTheme = () => {
         darkTheme.disabled = !darkTheme.disabled
         defaultTheme.disabled = !darkTheme.disabled
         document.cookie = `MDARK=${darkTheme.disabled ? 'N' : 'Y'}; path=/;`
     }
+
+    toggleThemeBtn.addEventListener('click', changeTheme)
 }
