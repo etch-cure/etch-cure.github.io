927b7ac135a082bcd69f622d923de88820f9b1ce
minha <etchcure@gmail.com>
2021-10-30 17:03:50 +0900

    [다크모드] 코드 리펙토링 및 아이콘 추가
    - 다크모드 스크립트 리펙토링
    - 토글 버튼 css 정리
    - 구글 웹폰트 아이콘 추가
    - after_footer_script로 변경

--- assets/js/custom/dark-theme.js
+++ assets/js/custom/dark-theme.js
@@ -1,34 +1,30 @@
-var defaultTheme = [...document.styleSheets].find(style => /(main.css)$/.test(style.href))
-var darkTheme = [...document.styleSheets].find(style => /(main_dark.css)$/.test(style.href))
+const defaultTheme = [...document.styleSheets].find(style => /(main.css)$/.test(style.href));
+const darkTheme = [...document.styleSheets].find(style => /(main_dark.css)$/.test(style.href));
+
+let setDarkMode = (isDark) => {
+    darkTheme.disabled = isDark !== true;
+    defaultTheme.disabled = isDark === true;
+    localStorage.setItem('theme', isDark ? 'dark' : 'default');
+}
 
 if (darkTheme) {
-    const darkModeCookie = document.cookie
-        .split('; ')
-        .find(co => co.startsWith('MDARK='))
-    if (darkModeCookie !== undefined) {
-        const dmodeValue = darkModeCookie.split('=')[1]
-        darkTheme.disabled = dmodeValue !== 'Y'
-        defaultTheme.disabled = dmodeValue === 'Y'
+    let currentTheme = localStorage.getItem('theme');
+    let isDarkMode = false;
+    if (currentTheme) {
+        isDarkMode = currentTheme === 'dark';
     } else {
-        if (matchMedia('(prefers-color-scheme: dark)').matches) {
-            darkTheme.disabled = false
-            defaultTheme.disabled = true
-        } else {
-            darkTheme.disabled = true
-            defaultTheme.disabled = false
-        }
-        document.cookie = `MDARK=${darkTheme.disabled ? 'N' : 'Y'}; path=/;`
+        isDarkMode = matchMedia('(prefers-color-scheme: dark)').matches;
     }
+
+    setDarkMode(isDarkMode);
 
     let toggleThemeBtn = document.getElementById("toggle_dark_theme")
     if (toggleThemeBtn) {
-        toggleThemeBtn.checked = defaultTheme.disabled
+        toggleThemeBtn.checked = isDarkMode
     }
 
-    let changeTheme = () => {
-        darkTheme.disabled = !darkTheme.disabled
-        defaultTheme.disabled = !darkTheme.disabled
-        document.cookie = `MDARK=${darkTheme.disabled ? 'N' : 'Y'}; path=/;`
+    let changeTheme = (e) => {
+        setDarkMode(e.target.checked);
     }
 
     toggleThemeBtn.addEventListener('click', changeTheme)
